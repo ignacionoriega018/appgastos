17eea4e720566e48c22190a227e48d6d6a2472c5
ignacionoriega018 <ignacio.noriega@eset.com>
2025-11-20 13:26:45 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/appgastos/', // 👈 IMPORTANTE PARA GITHUB PAGES
+  base: '/appgastos/',
+  build: {
+    outDir: 'dist',
+  },
 })
